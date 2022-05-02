477f86a0a5d5281db8e7b26633589cb05b071986
Rami Hkiri <hkirirami@gmail.com>
2022-05-02 22:41:17 +0100

    making a route and link to it

---
+++ resources/views/post.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<title>My blog</title>
+<link rel="stylesheet" href="/app.css">
+<body>
+    <article>
+        <h1><a href="/post">My first post</a></h1>
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime tenetur necessitatibus sint doloribus qui aperiam non, adipisci aspernatur debitis esse, perspiciatis ipsam ex nam magnam! Totam eius distinctio voluptatem vero.
+            Nemo doloribus veniam quisquam illum ut ex laborum aut! Repellat perferendis voluptatum animi harum ut, perspiciatis aut voluptatem a autem recusandae deleniti fugiat nulla libero! Repellendus dolor aut ratione exercitationem.
+            Quibusdam soluta corporis commodi, facilis hic necessitatibus ea assumenda sapiente cum rerum dolor doloremque vel maxime ad illum tempore sed, magnam labore? Neque, suscipit voluptatibus corrupti pariatur minus ut aperiam!
+
+        </p>
+    </article>
+    <a href="/">Home page</a>
+</body>
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<title>My blog</title>
+<link rel="stylesheet" href="/app.css">
+<body>
+    <article>
+        <h1><a href="/post">My first post</a></h1>
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime tenetur necessitatibus sint doloribus qui aperiam non, adipisci aspernatur debitis esse, perspiciatis ipsam ex nam magnam! Totam eius distinctio voluptatem vero.
+            Nemo doloribus veniam quisquam illum ut ex laborum aut! Repellat perferendis voluptatum animi harum ut, perspiciatis aut voluptatem a autem recusandae deleniti fugiat nulla libero! Repellendus dolor aut ratione exercitationem.
+            Quibusdam soluta corporis commodi, facilis hic necessitatibus ea assumenda sapiente cum rerum dolor doloremque vel maxime ad illum tempore sed, magnam labore? Neque, suscipit voluptatibus corrupti pariatur minus ut aperiam!
+
+        </p>
+    </article>
+    <article>
+        <h1><a href="/post">My Second post</a></h1>
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime tenetur necessitatibus sint doloribus qui aperiam non, adipisci aspernatur debitis esse, perspiciatis ipsam ex nam magnam! Totam eius distinctio voluptatem vero.
+            Nemo doloribus veniam quisquam illum ut ex laborum aut! Repellat perferendis voluptatum animi harum ut, perspiciatis aut voluptatem a autem recusandae deleniti fugiat nulla libero! Repellendus dolor aut ratione exercitationem.
+            Quibusdam soluta corporis commodi, facilis hic necessitatibus ea assumenda sapiente cum rerum dolor doloremque vel maxime ad illum tempore sed, magnam labore? Neque, suscipit voluptatibus corrupti pariatur minus ut aperiam!
+
+        </p>
+    </article>
+    <article>
+        <h1><a href="/post">My Third post</a></h1>
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime tenetur necessitatibus sint doloribus qui aperiam non, adipisci aspernatur debitis esse, perspiciatis ipsam ex nam magnam! Totam eius distinctio voluptatem vero.
+            Nemo doloribus veniam quisquam illum ut ex laborum aut! Repellat perferendis voluptatum animi harum ut, perspiciatis aut voluptatem a autem recusandae deleniti fugiat nulla libero! Repellendus dolor aut ratione exercitationem.
+            Quibusdam soluta corporis commodi, facilis hic necessitatibus ea assumenda sapiente cum rerum dolor doloremque vel maxime ad illum tempore sed, magnam labore? Neque, suscipit voluptatibus corrupti pariatur minus ut aperiam!
+
+        </p>
+    </article>
+</body>
+</html>

453a04bdd305e8470a02cca34252d9e97fcc3274
Rami Hkiri <hkirirami@gmail.com>
2022-05-04 20:05:36 +0100

    replace post array with post collection

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,29 @@
 <?php
 
 use App\Models\Post;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
-use Illuminate\Support\Facades\File;
 
 //home page
 Route::get('/', function () {
-    $files= File::files(resource_path("posts"));
-    $posts=[];
-    foreach ($files as $file) {
-        $document=YamlFrontMatter::parseFile($file);
-        $posts[]=new Post(
-            $document->title,
-            $document->slug,
-            $document->excerpt,
-            $document->date,
-            $document->body()
-            
-        );
-    }    
-     return view('posts', [
-        'posts' => $posts
-    ]); 
+    $posts = collect(File::files(resource_path("posts")))
+        ->map(function ($file) {
+            $document = YamlFrontMatter::parseFile($file);
+
+            return new Post(
+                $document->title,
+                $document->slug,
+                $document->excerpt,
+                $document->date,
+                $document->body()
+            );
+
+        });
+
+    return view('posts', [
+        'posts' => $posts,
+    ]);
 });
 // route to selected post
 Route::get('posts/{post}', function ($slug) {

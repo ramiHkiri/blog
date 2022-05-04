b1b157d2bdba4314adbb41420ff989f964abc21f
Rami Hkiri <hkirirami@gmail.com>
2022-05-04 10:02:01 +0100

    store posts as html files

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,7 +3,7 @@
 <link rel="stylesheet" href="/app.css">
 <body>
     <article>
-        <h1><a href="/post">My first post</a></h1>
+        <h1><a href="/posts/my-first-post">My first post</a></h1>
         <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime tenetur necessitatibus sint doloribus qui aperiam non, adipisci aspernatur debitis esse, perspiciatis ipsam ex nam magnam! Totam eius distinctio voluptatem vero.
             Nemo doloribus veniam quisquam illum ut ex laborum aut! Repellat perferendis voluptatum animi harum ut, perspiciatis aut voluptatem a autem recusandae deleniti fugiat nulla libero! Repellendus dolor aut ratione exercitationem.
@@ -12,7 +12,7 @@
         </p>
     </article>
     <article>
-        <h1><a href="/post">My Second post</a></h1>
+        <h1><a href="/posts/my-second-post">My Second post</a></h1>
         <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime tenetur necessitatibus sint doloribus qui aperiam non, adipisci aspernatur debitis esse, perspiciatis ipsam ex nam magnam! Totam eius distinctio voluptatem vero.
             Nemo doloribus veniam quisquam illum ut ex laborum aut! Repellat perferendis voluptatum animi harum ut, perspiciatis aut voluptatem a autem recusandae deleniti fugiat nulla libero! Repellendus dolor aut ratione exercitationem.
@@ -21,7 +21,7 @@
         </p>
     </article>
     <article>
-        <h1><a href="/post">My Third post</a></h1>
+        <h1><a href="/posts/my-third-post">My Third post</a></h1>
         <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime tenetur necessitatibus sint doloribus qui aperiam non, adipisci aspernatur debitis esse, perspiciatis ipsam ex nam magnam! Totam eius distinctio voluptatem vero.
             Nemo doloribus veniam quisquam illum ut ex laborum aut! Repellat perferendis voluptatum animi harum ut, perspiciatis aut voluptatem a autem recusandae deleniti fugiat nulla libero! Repellendus dolor aut ratione exercitationem.
